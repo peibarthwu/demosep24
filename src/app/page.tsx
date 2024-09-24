4ebb6f0b94bbb292f3a874eebf72766c2c270df0
peibarthwu <peibarthwu@gmail.com>
2024-09-24 11:15:16 -0400

    intiial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { Intro, Scene1, ThreeScene } from "../components";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+<>
+<ThreeScene />
+    </>
+  );
+}
